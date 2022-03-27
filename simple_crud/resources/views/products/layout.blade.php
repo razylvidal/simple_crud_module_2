06360cf79ddb68f0615c7bb9f96d2dfb0560e23e
razylvidal <razylabbygail.vidal@unc.edu.ph>
2022-03-27 18:35:17 +0800

    first commit

---
+++ simple_crud/resources/views/products/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <title>Simple CRUD </title>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
